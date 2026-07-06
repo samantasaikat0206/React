c5e2a7b5da5c69daba4d2b8a53e2a283e5d3dc2b
Saikat Samanta <samantasaikat0206@gmail.com>
2026-07-07 01:14:03 +0530

    create function based component and add it inti the App.jsx file

--- 2_React_basic/src/App.jsx
+++ 2_React_basic/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ClassBasedComponent from './components/ClassBasedComponent'
+import FunctionBasedComponent from './components/FunctionBasedComponent'
 
 const App = () => {
   return (
     <div>
       <h1>My React App</h1>
       <ClassBasedComponent/>
+      <FunctionBasedComponent/>
     </div>
   )
 }
---
+++ 2_React_basic/src/components/FunctionBasedComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FunctionBasedComponent = () => {
+  return (
+    <div>
+        <h2>Function-Based Component</h2>
+    </div>
+  )
+}
+
+export default FunctionBasedComponent
