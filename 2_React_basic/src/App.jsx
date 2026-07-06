3d0cf27b3d807fda6945ac6a4b1f7027eef84800
Saikat Samanta <samantasaikat0206@gmail.com>
2026-07-07 01:09:30 +0530

    add class based component in App.jsx

--- 2_React_basic/src/App.jsx
+++ 2_React_basic/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import ClassBasedComponent from './components/ClassBasedComponent'
 
 const App = () => {
   return (
     <div>
       <h1>My React App</h1>
+      <ClassBasedComponent/>
     </div>
   )
 }
