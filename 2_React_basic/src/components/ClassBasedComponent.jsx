937bd18509205b4c832d0747ce90b6f76b14b014
Saikat Samanta <samantasaikat0206@gmail.com>
2026-07-07 01:08:47 +0530

    class base component make

---
+++ 2_React_basic/src/components/ClassBasedComponent.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class ClassBasedComponent extends Component {
+    render(){
+        return (
+            <div>   
+                <h2>Class-Based Component</h2>
+            </div>
+        )
+    }
+}
+
+export default ClassBasedComponent;
